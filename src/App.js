6517da4764696c63dba49f1ffc06599661ae1b5e
unknown <samehrizk1993@gmail.com>
2025-02-12 01:35:12 +0200

    small update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
+import Sidebar from "./component/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="row">
-        <div className="col-2">test</div>
-        <div className="col-10">test2</div>
+        <div className="col-3 sidebar">
+          <Sidebar />
+        </div>
+        <div className="col-9">home page</div>
       </div>
     </div>
   );
---
+++ src/component/Sidebar.js
@@ -0,0 +1,15 @@
+function Sidebar() {
+  return (
+    <>
+      <ul className="list-unstyled">
+        <li>
+          <a href="#"> Get All Products</a>
+        </li>
+        <li>
+          <a href="#"> Get All Categories</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+export default Sidebar;
