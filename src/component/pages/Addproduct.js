c09689a0c8e6e2f908731b2baab137f45aa4e8c4
unknown <samehrizk1993@gmail.com>
2025-02-16 01:05:55 +0200

    adding form

--- src/component/pages/Addproduct.js
+++ src/component/pages/Addproduct.js
@@ -1,5 +1,52 @@
 function Addproduct() {
-  return <h1>add product</h1>;
+  const formSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <h1>Add Product</h1>
+      <form onSubmit={formSubmit}>
+        <div className="mb-3">
+          <label htmlFor="productTitle" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="productTitle"
+            placeholder="Product Title"
+            aria-describedby="Product Title"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="productPrice" className="form-label">
+            Price
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="productPrice"
+            placeholder="Product Price"
+            aria-describedby="Product Price"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Add Product
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Addproduct;
